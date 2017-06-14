3322ebc101e7bde35ec96fa108347f4b9c270612
Digoritos <rmc.morelli@gmail.com>
2017-06-14 14:15:35 -0300

    Add files via upload

---
+++ offline.js
@@ -0,0 +1,21 @@
+﻿{
+	"version": 1496703647,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-2.1.1.min.js",
+		"offlineClient.js",
+		"images/tiledbackground.png",
+		"images/tiledbackground2.png",
+		"images/tiledbackground3.png",
+		"images/tiledbackground4.png",
+		"images/tiledbackground5.png",
+		"images/tiledbackground6.png",
+		"images/estala_b_spawner-sheet0.png",
+		"images/estala_b-sheet0.png",
+		"images/gema_spawner-sheet0.png",
+		"images/sprite-sheet0.png",
+		"images/pedra-sheet0.png",
+		"images/player-sheet0.png"
+	]
+}
